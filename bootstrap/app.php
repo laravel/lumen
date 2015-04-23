d1b9f866c30a8d93e8a118effd50b54dd39fd90e
Taylor Otwell <taylorotwell@gmail.com>
2015-04-22 22:06:08 -0500

    Just pass the base path.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = new Laravel\Lumen\Application;
+$app = new Laravel\Lumen\Application(
+	realpath(__DIR__.'/../')
+);
 
 // $app->withFacades();
 
