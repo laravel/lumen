6c96808d2ba08e87c108f366a5d7b7b695237517
Nicholas DeVries <ndevriesmail@icloud.com>
2015-06-17 08:25:30 -0500

    psr2 fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,11 +1,12 @@
-<?php namespace App\Console;
+<?php
+
+namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-
     /**
      * The Artisan commands provided by your application.
      *
