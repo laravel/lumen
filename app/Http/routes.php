0d65c0c2958f703980fd9f11f66339b772845416
Taylor Otwell <taylorotwell@gmail.com>
2015-06-29 16:28:16 -0500

    wording

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,8 +6,8 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
 |
 */
 
