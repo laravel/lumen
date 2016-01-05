8db669a1ed9ffb657d508f1c1c223e94c2b9928d
Taylor Otwell <taylorotwell@gmail.com>
2016-01-05 14:31:07 -0600

    Example classes.

---
+++ app/Events/ExampleEvent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Events;
+
+class ExampleEvent extends Event
+{
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    	//
+    }
+}
