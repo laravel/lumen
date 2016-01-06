4dc0510084203cc843c5c1acd99a064524ee3968
Taylor Otwell <taylorotwell@gmail.com>
2016-01-06 14:06:05 -0600

    Working on auth.

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Here you may define how you wish users to be authenticated for your Lumen
+        // application. The callback which receives the incoming request instance
+        // should return either a User instance or null. You're free to obtain
+        // the User instance via an API token or any other method necessary.
+
+        Auth::viaRequest('api', function ($request) {
+            if ($request->api_token) {
+                return User::where('api_token', $request->api_token)->first();
+            }
+        });
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,6 +75,7 @@ $app->singleton(
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
+// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
