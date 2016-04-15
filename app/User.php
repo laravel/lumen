1239256de8ec6d37a8df3471aa1f8cc46b27a456
Graham Campbell <graham@alt-three.com>
2016-04-15 18:32:44 -0400

    Applied fixes from StyleCI

--- app/User.php
+++ app/User.php
@@ -8,9 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements
-    AuthenticatableContract,
-    AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable;
 
