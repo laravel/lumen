999a08152dd8f88797a92c24a75bdfe1cad2cb50
Jelle Breuer <jelle@breuclan.nl>
2017-03-16 16:11:59 +0100

    Style fixes 2

--- app/Http/Middleware/ThrottleRequests.php
+++ app/Http/Middleware/ThrottleRequests.php
@@ -59,9 +59,9 @@ class ThrottleRequests
     protected function resolveRequestSignature($request)
     {
         return sha1(
-            $request->method() .
-            '|'.$request->server('SERVER_NAME') .
-            '|'.$request->path() .
+            $request->method().
+            '|'.$request->server('SERVER_NAME').
+            '|'.$request->path().
             '|'.$request->ip()
         );
     }
@@ -125,5 +125,4 @@ class ThrottleRequests
 
         return 0;
     }
-
 }
