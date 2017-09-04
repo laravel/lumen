6cb6191a1e862973f4f9d81aa6c36f594e878611
Mohamed Said <themohamedsaid@gmail.com>
2017-09-04 19:13:40 +0200

    update for lumen 5.5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -93,7 +93,7 @@ $app->singleton(
 |
 */
 
-$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
+$app->router->group(['namespace' => 'App\Http\Controllers'], function ($router) {
     require __DIR__.'/../routes/web.php';
 });
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->version();
+$router->get('/', function () use ($router) {
+    return $router->app->version();
 });
