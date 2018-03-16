65efe4fb9b72f6c06981b12be4b3118675396200
Chimit <chimit@users.noreply.github.com>
2018-03-16 12:32:49 +0800

    Set an existing example event and example listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,8 +12,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
+        'App\Events\ExampleEvent' => [
+            'App\Listeners\ExampleListener',
         ],
     ];
 }
