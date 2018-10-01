ce15b9d040a50d1848a6bc5ce66cc755083c629f
Dries Vints <dries.vints@gmail.com>
2018-10-01 13:54:39 +0200

    Fix indentation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,7 +60,7 @@ $app->singleton(
 */
 
 // $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
+//     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
 // $app->routeMiddleware([
