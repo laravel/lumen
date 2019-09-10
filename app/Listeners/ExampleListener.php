1e933da7bd69235d32a51660dd59e3484ccd539f
Dries Vints <dries.vints@gmail.com>
2019-09-10 16:12:55 +0000

    Apply fixes from StyleCI

--- app/Listeners/ExampleListener.php
+++ app/Listeners/ExampleListener.php
@@ -3,8 +3,8 @@
 namespace App\Listeners;
 
 use App\Events\ExampleEvent;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
 
 class ExampleListener
 {
