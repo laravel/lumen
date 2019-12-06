04f3fc7a651e26ad00337de71c794f7ade11e8ee
Gege <dngege@gmail.com>
2019-12-06 14:32:37 +0100

    use FQCNs

--- app/Listeners/ExampleListener.php
+++ app/Listeners/ExampleListener.php
@@ -21,7 +21,7 @@ class ExampleListener
     /**
      * Handle the event.
      *
-     * @param  ExampleEvent  $event
+     * @param  \App\Events\ExampleEvent  $event
      * @return void
      */
     public function handle(ExampleEvent $event)
