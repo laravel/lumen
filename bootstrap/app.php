d7dd4152367dd6f0dbe0407c849bdd768cfe5e44
Alex <alexk.mammut@gmail.com>
2021-01-21 11:45:46 +0200

    Commented default namespace

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -107,7 +107,7 @@ $app->configure('app');
 */
 
 $app->router->group([
-    'namespace' => 'App\Http\Controllers',
+//    'namespace' => 'App\Http\Controllers',
 ], function ($router) {
     require __DIR__.'/../routes/web.php';
 });
