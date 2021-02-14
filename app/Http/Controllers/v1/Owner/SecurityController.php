e63f6d7ca2ab92f8425c783040ddebbb90ae1bac
Farhan Zuhdi <fzuhdi50@gmail.com>
2021-02-14 18:30:59 +0700

    Update Route, AuthController And Add SecurityController, SecurityScheduleController.php

--- app/Http/Controllers/v1/Owner/SecurityController.php
+++ app/Http/Controllers/v1/Owner/SecurityController.php
@@ -11,14 +11,19 @@ use Illuminate\Support\Facades\Validator;
 class SecurityController extends Controller
 {
     /**
-     * Show list data
+     * Get Security data
+     * GET api/v1/owner/security/
+     * @param id
+     * @return Response
+     * @
      **/
-	public function index()
+	public function index($id = NULL)
 	{
         try 
         {
-            $security = Security::get();
-            return $this->respHandler->success($security);
+            $security = $id ? Security::find($id) : Security::get();
+
+            return $this->respHandler->success('Success get data.', $security);
         } 
         catch(\Exception $e)
         {
@@ -27,13 +32,89 @@ class SecurityController extends Controller
 	}
 
     /**
-     * Store data request
+     * Create new Security
+     * POST api/v1/owner/security/store
+     * @param Request id_people
+     * @param Request id_supervisor
+     * @param Request security_number
+     * @return Response
+     * @
      **/
-    public function store(Request $request)
+    public function storeSecurity(Request $request)
 	{
         try 
         {
-            // Start code here
+            $validator = Validator::make($request->post(), [
+                'id_people' => 'required'
+            ]);
+            
+            if($validator->validated())
+            {
+                $new_security = new Security;
+                $new_security->id_people = $request->id_people;
+                $new_security->id_supervisor = $request->id_supervisor;
+                $new_security->security_number = $request->security_number;
+                $new_security->save();
+
+                return $this->respHandler->success('Security has been saved.', $new_security);
+            }
+            else
+                return $this->respHandler->requestError($validator->errors());
+        } 
+        catch(\Exception $e)
+        {
+            return $this->respHandler->requestError($e->getMessage());
+        }
+	}
+
+    /**
+     * Update Security
+     * POST api/v1/owner/security/update
+     * @param Request id
+     * @param Request id_supervisor
+     * @return Response
+     **/
+    public function updateSecurity(Request $request)
+	{
+        try 
+        {
+            // dd($request->post() );
+            $validator = Validator::make($request->post(), [
+                'id' => 'required',
+                'id_supervisor' => 'required',
+                'security_number' => 'required'
+            ]);
+            
+            if($validator->validated())
+            {
+                $new_security = Security::find($request->id);
+                $new_security->id_supervisor = $request->id_supervisor;
+                $new_security->security_number = $request->security_number;
+                $new_security->save();
+
+                return $this->respHandler->success('Security has been updated.', $new_security);
+            }
+            else
+                return $this->respHandler->requestError($validator->errors());
+        } 
+        catch(\Exception $e)
+        {
+            return $this->respHandler->requestError($e->getMessage());
+        }
+	}
+
+    /**
+     * Delete Security
+     * GET api/v1/owner/security/store
+     * @param id
+     **/
+    public function deleteSecurity($id)
+	{
+        try 
+        {
+            $security = Security::find($id)->delete();
+
+            return $this->respHandler->success('Security has been deleted.');
         } 
         catch(\Exception $e)
         {
