12b0dc70c94e203aafbe24fa95bebfc5a2d8edb4
Ahmad Shidqi Firdaus <32896532+ahmadkikok@users.noreply.github.com>
2021-02-24 17:24:50 +0700

    Update 2222_22_22_222222_foreign_keys.php

--- database/migrations/2222_22_22_222222_foreign_keys.php
+++ database/migrations/2222_22_22_222222_foreign_keys.php
@@ -14,50 +14,50 @@ class ForeignKeys extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade');
-            $table->foreign('id_corporate')->references('id')->on('corporates')->onDelete('cascade');
+            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_corporate')->references('id')->on('corporates')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('messages', function (Blueprint $table) {
-            $table->foreign('id_report')->references('id')->on('reports')->onDelete('cascade');
-            $table->foreign('id_report_detail')->references('id')->on('report_details')->onDelete('cascade');
-            $table->foreign('id_respondent')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_report')->references('id')->on('reports')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_report_detail')->references('id')->on('report_details')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_respondent')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('reports', function (Blueprint $table) {
-            $table->foreign('id_security_real')->references('id')->on('securities')->onDelete('cascade');
-            $table->foreign('id_security_schedule')->references('id')->on('security_schedules')->onDelete('cascade');
+            $table->foreign('id_security_real')->references('id')->on('securities')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_security_schedule')->references('id')->on('security_schedules')->onDelete('cascade')->onUpdate('cascade');
         });
         Schema::table('report_details', function (Blueprint $table) {
-            $table->foreign('id_report')->references('id')->on('reports')->onDelete('cascade');
-            $table->foreign('id_checkpoint')->references('id')->on('checkpoints')->onDelete('cascade');
+            $table->foreign('id_report')->references('id')->on('reports')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_checkpoint')->references('id')->on('checkpoints')->onDelete('cascade')->onUpdate('cascade');
         });
         
         Schema::table('securities', function (Blueprint $table) {
-            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade');
-            $table->foreign('id_supervisor')->references('id')->on('securities')->onDelete('cascade');
+            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_supervisor')->references('id')->on('securities')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('security_schedules', function (Blueprint $table) {
-            $table->foreign('id_site_schedule')->references('id')->on('site_schedules')->onDelete('cascade');
-            $table->foreign('id_security_plan')->references('id')->on('securities')->onDelete('cascade');
+            $table->foreign('id_site_schedule')->references('id')->on('site_schedules')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_security_plan')->references('id')->on('securities')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('site_schedules', function (Blueprint $table) {
-            $table->foreign('id_site')->references('id')->on('sites')->onDelete('cascade');
-            $table->foreign('id_schedule')->references('id')->on('schedules')->onDelete('cascade');
+            $table->foreign('id_site')->references('id')->on('sites')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_schedule')->references('id')->on('schedules')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('sites', function (Blueprint $table) {
-            $table->foreign('id_corporate')->references('id')->on('corporates')->onDelete('cascade');
+            $table->foreign('id_corporate')->references('id')->on('corporates')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('checkpoints', function (Blueprint $table) {
-            $table->foreign('id_site')->references('id')->on('sites')->onDelete('cascade');
+            $table->foreign('id_site')->references('id')->on('sites')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade');
+            $table->foreign('id_people')->references('id')->on('peoples')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
