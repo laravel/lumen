45bb979e7d823bf3665dead845a65fedb5d53935
Kiran Maniya <vkiranmaniya@gmail.com>
2021-11-16 16:26:55 +0530

    Added custom artisan command example file
    Stub file for custom artisan command

---
+++ app/Console/Commands/ExampleCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ExampleCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Write a description for command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        
+    }
+}
